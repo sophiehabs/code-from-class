c608ea9bed942d8e81f11b3d1b5961980de0931a
Pepe García <pepe@pepegar.com>
2023-01-12 15:44:07 +0100

    session 2

--- client.py
+++ client.py
@@ -1,8 +1,15 @@
+#%%
 import requests
 
-response = requests.get("http://google.com")
+#response = requests.get("http://google.com")
 
-print(response.text)
+#print(response.text)
+
+response = requests.get("https://swapi.dev/api/people/1/")
+
+luke = response.json()
+
+print(luke["height"])
 
 # Next, get the information about Luke Skywalker from SWAPI!
 #
@@ -12,3 +19,5 @@ print(response.text)
 #
 # Hint: you'll probably need to access the json body of the response with the
 # json() method
+
+# %%
--- server.py
+++ server.py
@@ -1,4 +1,29 @@
 # 0. import flask
+from flask import Flask
+
 # 1. create the flask application
+app = Flask("server")
+
+@app.route("/")
+def index():
+    return """
+    <h1>Hello MCSBT</h1>
+    <p>
+        <a href="/potato">link</a>
+    </p>
+    """
+
+@app.route("/potato")
+def jj():
+    return "Hey y'all"
+
+@app.route("/users/<user>")
+def say_hello(user):
+    if user == "pepe":
+        return "hey Professor"
+    else:
+        return "hello " + user
+
 # 2. create a route
 # 3. run the application
+app.run()
